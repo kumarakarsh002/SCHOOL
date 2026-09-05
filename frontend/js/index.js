478e1bdf48c9f7d3f4f987947fa4920e91bae3bc
AKARSH KUMAR <kumarakarsh002@gmail.com>
2026-09-05 20:52:19 +0530

    checking requests

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -8,15 +8,7 @@ function Student() {
 
 // Our Top student
 
-const student = [
-    {
-        id: 1,
-        image: "./image/home/topper.png",
-        studentmarks: 95.6,
-        studentname: "",
-        studentclass: "",
-    }
-]
+
 
 
 
@@ -27,55 +19,55 @@ const student = [
 const review = [
     {
         id: 1,
-        image: "https://thumbs.dreamstime.com/b/d-cartoon-happy-man-showing-thumbs-up-face-very-male-leader-carrying-backpack-confidently-both-hands-isolated-white-333857675.jpg",
+        image: "./image/home/review.png",
         stars: 5,
         text: "There is nothing to do"
     },
     {
         id: 2,
-        image: "https://thumbs.dreamstime.com/b/d-cartoon-happy-man-showing-thumbs-up-face-very-male-leader-carrying-backpack-confidently-both-hands-isolated-white-333857675.jpg",
+        image: "./image/home/review.png",
         stars: 5,
         text: "There is nothing to do"
     },
     {
         id: 3,
-        image: "https://thumbs.dreamstime.com/b/d-cartoon-happy-man-showing-thumbs-up-face-very-male-leader-carrying-backpack-confidently-both-hands-isolated-white-333857675.jpg",
+        image: "./image/home/review.png",
         stars: 5,
         text: "There is nothing to do"
     },
     {
         id: 4,
-        image: "https://thumbs.dreamstime.com/b/d-cartoon-happy-man-showing-thumbs-up-face-very-male-leader-carrying-backpack-confidently-both-hands-isolated-white-333857675.jpg",
+        image: "./image/home/review.png",
         stars: 5,
         text: "There is nothing to do"
     },
     {
         id: 5,
-        image: "https://thumbs.dreamstime.com/b/d-cartoon-happy-man-showing-thumbs-up-face-very-male-leader-carrying-backpack-confidently-both-hands-isolated-white-333857675.jpg",
+        image: "./image/home/review.png",
         stars: 5,
         text: "There is nothing to do"
     },
     {
         id: 6,
-        image: "https://thumbs.dreamstime.com/b/d-cartoon-happy-man-showing-thumbs-up-face-very-male-leader-carrying-backpack-confidently-both-hands-isolated-white-333857675.jpg",
+        image: "./image/home/review.png",
         stars: 5,
         text: "There is nothing to do"
     },
     {
         id: 7,
-        image: "https://thumbs.dreamstime.com/b/d-cartoon-happy-man-showing-thumbs-up-face-very-male-leader-carrying-backpack-confidently-both-hands-isolated-white-333857675.jpg",
+        image: "./image/home/review.png",
         stars: 5,
         text: "There is nothing to do"
     },
     {
         id: 8,
-        image: "https://thumbs.dreamstime.com/b/d-cartoon-happy-man-showing-thumbs-up-face-very-male-leader-carrying-backpack-confidently-both-hands-isolated-white-333857675.jpg",
+        image: "./image/home/review.png",
         stars: 5,
         text: "There is nothing to do"
     },
     {
         id: 9,
-        image: "https://thumbs.dreamstime.com/b/d-cartoon-happy-man-showing-thumbs-up-face-very-male-leader-carrying-backpack-confidently-both-hands-isolated-white-333857675.jpg",
+        image: "./image/home/review.png",
         stars: 5,
         text: "There is nothing to do"
     },
